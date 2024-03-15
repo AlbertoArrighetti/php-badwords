6890087accab57390c9a66bc91f058071135a88f
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-03-15 15:22:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1>Hello word</h1>
+</body>
+</html>
